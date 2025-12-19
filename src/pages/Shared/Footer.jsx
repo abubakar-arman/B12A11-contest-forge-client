41d7aeb7cf3d312336dcd6cab61013ca28a4fe9f
abubakar-arman <abubakar.arman.cse@gmail.com>
2025-12-20 03:14:55 +0600

    Add Footer

---
+++ src/pages/Shared/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Logo from '../../Components/Logo';
+import { FaFacebookSquare, FaLinkedinIn } from "react-icons/fa";
+
+const Footer = () => {
+    return (
+        <footer className="footer footer-horizontal footer-center bg-primary/60 text-primary-content p-10">
+            <aside>
+                <Logo />
+                <p className="font-bold">
+                    Craft Greatness. Launch Contests.
+                </p>
+                <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+            </aside>
+            <nav>
+                <div className="grid grid-flow-col gap-4">
+                    <a>
+                        <FaFacebookSquare className='size-8' />
+                    </a>
+                    <a>
+                        <FaLinkedinIn className='size-8' />
+                    </a>
+                </div>
+            </nav>
+        </footer>
+    );
+};
+
+export default Footer;
