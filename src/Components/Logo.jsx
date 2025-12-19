7e672d515d331971802055276d09485061adc5c9
abubakar-arman <abubakar.arman.cse@gmail.com>
2025-12-20 02:56:57 +0600

    Add Navbar

---
+++ src/Components/Logo.jsx
@@ -0,0 +1,12 @@
+import logo from '../assets/logo.png'
+
+const Logo = () => {
+    return (
+        <div className='flex items-center justify-center'>
+            <img src={logo} className='w-8' alt="" />
+            <h2 className='font-extrabold text-lg pt-5 text-orange-700/80'>CONTEST <span className='text-[#314048]'>FORGE</span></h2>
+        </div>
+    );
+};
+
+export default Logo;
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import Navbar from '../pages/Shared/Navbar';
 
 const RootLayout = () => {
     return (
         <div>
+            <Navbar />
             <Outlet />
         </div>
     );
