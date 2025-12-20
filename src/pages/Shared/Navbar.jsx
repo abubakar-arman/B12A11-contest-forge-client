3eccbe6591cb23a120ee16e41ebef5de69e808cc
abubakar-arman <abubakar.arman.cse@gmail.com>
2025-12-21 00:59:04 +0600

    Add UserCard and theming

--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Logo from '../../Components/Logo';
-import { NavLink } from 'react-router';
+import { NavLink, useNavigate } from 'react-router';
+import useAuth from '../../hooks/useAuth';
+import { MdDarkMode } from "react-icons/md";
+import { MdLightMode } from "react-icons/md";
 
 const Navbar = () => {
+    const { logout, isAuthenticated, user } = useAuth()
+    const navigate = useNavigate()
+    const [theme, setTheme] = useState(localStorage.getItem("theme") || "light")
+
+    useEffect(() => {
+        const html = document.querySelector('html')
+        html.setAttribute('data-theme', theme)
+        localStorage.setItem('theme', theme)
+    }, [theme])
+
+    const handleLogout = () => {
+        logout();
+        navigate('/')
+    }
+
+    const handleTheme = checked => {
+        const theme = checked ? 'dark' : 'light'
+        setTheme(theme)
+    }
+
+
     const navLinks = <>
         <li key={1}>
             <NavLink to='/' >Home</NavLink>
@@ -42,7 +66,29 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className="navbar-end">
+                
+                {isAuthenticated &&
+                    <div className="dropdown">
 
+                        <div tabIndex={0} role='button' className="avatar mr-4">
+                            <div className="ring-primary ring-offset-base-100 w-10 rounded-full ring-2 ring-offset-2">
+                                <img src={user.photoURL} />
+                            </div>
+                        </div>
+                        <div
+                            tabIndex={0}
+                            className="dropdown-content card card-sm bg-base-100 z-100 w-64 shadow-md right-0">
+                            <div className="card-body">
+                                <p className='font-semibold text-xl'>{user.displayName}</p>
+                                <p>Email : {user.email}</p>
+                                <button onClick={handleLogout} className="btn bg-red-700 text-base-100">Logout</button>
+                            </div>
+                        </div>
+                    </div>
+                }
+                <div className="theme flex items-center gap-2">
+                <MdLightMode /><input onChange={(e) => handleTheme(e.target.checked)} checked={theme === 'dark' ? 'checked' : ''} type="checkbox" className="toggle" /><MdDarkMode />
+                </div>
             </div>
         </div>
     );
