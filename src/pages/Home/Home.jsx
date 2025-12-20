b4dae05eba7caee50b610ea6bc7f8fe21116bab5
abubakar-arman <abubakar.arman.cse@gmail.com>
2025-12-21 02:49:00 +0600

    Add server connection

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import useAuth from '../../hooks/useAuth';
 
 const Home = () => {
     const {user} = useAuth()
+    const [msg, setMsg] = useState('')
+
     useEffect(() => {
         console.log(user);
     },[user])
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            // setIsLoading(true)
+            const res = await fetch('http://localhost:3000/')
+            const data = await res.json()
+            console.log(data);
+            setMsg(data)
+        }
+        fetchData()
+    }, [setMsg])
+
     return (
         <div>
             home
+            {msg && <p>Message : {msg.msg}</p>}
         </div>
     );
 };
