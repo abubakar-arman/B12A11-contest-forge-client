aec1994b835245cb60a516eaf844cb461456f10f
abubakar-arman <abubakar.arman.cse@gmail.com>
2026-02-10 03:26:11 +0600

    Add CreatedContests functionality

--- src/pages/Dashboard/Creator/CreatedContests.jsx
+++ src/pages/Dashboard/Creator/CreatedContests.jsx
@@ -1,30 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import React, { useState } from 'react';
 import { FaEdit, FaTrash } from 'react-icons/fa';
 import { IoIosOpen } from "react-icons/io";
 import { MdSubject } from "react-icons/md";
 import { Link } from 'react-router';
+import api from '../../../config/api';
+import { toast } from 'react-toastify';
+import useAuth from '../../../hooks/useAuth';
 
 const CreatedContests = () => {
-    const [contests, setContests] = useState([])
+    const {user} = useAuth()
+    const { data, isLoading, error } = useQuery({
+        queryKey: ['contests'],
+        queryFn: () => api.get(`/api/contests`),
+    })
+
+    const queryClient = useQueryClient();
+    const mutationDelete = useMutation({
+        mutationFn: (_id) => api.delete(`/api/contests/${_id}`),
+        onSuccess: (res) => {
+            console.log('Server Response :', res.data);
+            queryClient.invalidateQueries({ queryKey: ['contests'] })
+
+            toast.success('Contest deleted')
+        },
+        onError: (err) => console.error('Mutation Failed :', err)
+    })
+
     const [currentPage, setCurrentPage] = useState(1)
     const itemsPerPage = 10
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const res = await fetch('/api.json')
-            const data = await res.json()
-            setContests(data)
-        }
-        fetchData()
-    }, [setContests])
-    console.log('contests', contests);
+    if (isLoading) return <div className="text-center p-10">Loading contests...</div>;
+    if (error) return <p>Error: {error.message}</p>
+    const contests = data.data.result
+    const creatorContests = contests.filter(contest => contest.created_by == user.email)
 
     // pagination
-    const totalPages = Math.ceil(contests.length / itemsPerPage);
+    const totalPages = Math.ceil(creatorContests.length / itemsPerPage);
     const startIndex = (currentPage - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
-    const currentItems = contests.slice(startIndex, endIndex);
-
+    const currentItems = creatorContests.slice(startIndex, endIndex);
+    
     return (
         <div>
             <h3 className='text-3xl font-bold text-accent-content mb-5 text-center'>My Created Contests</h3>
@@ -39,6 +55,7 @@ const CreatedContests = () => {
                                 </label>
                             </th>
                             <th>Contest</th>
+                            <th>Created By</th>
                             <th>Deadline</th>
                             <th>Status</th>
                             <th>Actions</th>
@@ -57,7 +74,7 @@ const CreatedContests = () => {
                                         <div className="avatar">
                                             <div className="mask mask-squircle h-12 w-12">
                                                 <img
-                                                    src={`/posters/${contest.banner_url}`}
+                                                    src={`/posters/${contest.image}`}
                                                     alt="Avatar Tailwind CSS Component" />
                                             </div>
                                         </div>
@@ -68,14 +85,21 @@ const CreatedContests = () => {
                                     </div>
                                 </td>
                                 <td>
-                                    {contest.deadline.split('T')[0]}
+                                    {contest.created_by}
+                                </td>
+                                <td>
+                                    {contest.deadline?.split('T')[0]}
                                 </td>
                                 <td>Approved</td>
                                 <th className='flex gap-1'>
-                                    <Link to={`/contest-details/${contest.id}`} className="btn btn-primary btn-square tooltip" data-tip="Open"><IoIosOpen /></Link>
-                                    <button className="btn btn-primary btn-square tooltip" data-tip="Edit"><FaEdit /></button>
-                                    <button className="btn btn-primary btn-square tooltip" data-tip="Submissions"><MdSubject /></button>
-                                    <button className="btn btn-primary btn-square tooltip" data-tip="Delete"><FaTrash /></button>
+                                    <Link to={`/contest-details/${contest._id}`} className="btn btn-primary btn-square tooltip" data-tip="Open"><IoIosOpen /></Link>
+                                    <Link to={`/dashboard/update-contest/${contest._id}`} className="btn btn-primary btn-square tooltip" data-tip="Edit"><FaEdit /></Link>
+                                    <Link to={`/dashboard/submitted-tasks/${contest._id}`} className="btn btn-primary btn-square tooltip" data-tip="Submissions"><MdSubject /></Link>
+                                    <button
+                                        className="btn btn-primary btn-square tooltip"
+                                        data-tip="Delete"
+                                        onClick={() => mutationDelete.mutate(contest._id)}
+                                    ><FaTrash /></button>
                                 </th>
                             </tr>
                         ))}
@@ -85,6 +109,7 @@ const CreatedContests = () => {
                         <tr>
                             <th></th>
                             <th>Contest</th>
+                            <th>Created By</th>
                             <th>Deadline</th>
                             <th>Status</th>
                             <th>Actions</th>
