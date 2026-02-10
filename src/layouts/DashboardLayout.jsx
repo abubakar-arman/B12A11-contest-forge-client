dcae4de83ce1e9520b2a4575cfd11bcb9e58ea5e
abubakar-arman <abubakar.arman.cse@gmail.com>
2026-02-10 09:21:41 +0600

    Add navigation to dashboard

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FaTasks, FaUserCircle } from 'react-icons/fa';
-import { Link, Outlet } from 'react-router';
+import { Link, Outlet, useNavigate } from 'react-router';
 import { GiDiamondTrophy } from "react-icons/gi";
 import { GrTrophy } from "react-icons/gr";
 import { ToastContainer } from 'react-toastify';
@@ -8,11 +8,13 @@ import useAuth from '../hooks/useAuth';
 import { useQuery } from '@tanstack/react-query';
 import api from '../config/api';
 import { IoIosAddCircle } from 'react-icons/io';
-import { MdCreateNewFolder } from 'react-icons/md';
+import { MdCreateNewFolder, MdDarkMode, MdLightMode } from 'react-icons/md';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 
 const DashboardLayout = () => {
-    const {user} = useAuth()
+    const { user, isAuthenticated, logout } = useAuth()
     const { data, isLoading, error } = useQuery({
         queryKey: ['userData'],
         queryFn: () => api.get(`/api/user/find/` + user.email),
@@ -20,7 +22,26 @@ const DashboardLayout = () => {
     })
     // console.log(_id);
     
-    
+    const navigate = useNavigate()
+    const [theme, setTheme] = useState(localStorage.getItem("theme") || "light")
+
+    useEffect(() => {
+        const html = document.querySelector('html')
+        html.setAttribute('data-theme', theme)
+        localStorage.setItem('theme', theme)
+    }, [theme])
+
+    const handleLogout = () => {
+        logout();
+        navigate('/')
+    }
+
+    const handleTheme = checked => {
+        const theme = checked ? 'dark' : 'light'
+        setTheme(theme)
+    }
+
+
     if (isLoading) return <div className="text-center p-10">Loading user...</div>;
     if (error) return <p>Error: {error.message}</p>
     const userData = data?.data?.result
@@ -32,11 +53,38 @@ const DashboardLayout = () => {
             <div className="drawer-content">
                 {/* Navbar */}
                 <nav className="navbar w-full bg-base-300">
+                    <div className='navbar-start'>
                     <label htmlFor="my-drawer-4" aria-label="open sidebar" className="btn btn-square btn-ghost">
                         {/* Sidebar toggle icon */}
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg>
                     </label>
                     <div className="px-4">User Dashboard &gt; My Participated Contests</div>
+                    </div>
+                    <div className="navbar-end">
+
+                        {isAuthenticated &&
+                            <div className="dropdown">
+
+                                <div tabIndex={0} role='button' className="avatar mr-4">
+                                    <div className="ring-primary ring-offset-base-100 w-10 rounded-full ring-2 ring-offset-2">
+                                        <img src={user.photoURL} />
+                                    </div>
+                                </div>
+                                <div
+                                    tabIndex={0}
+                                    className="dropdown-content card card-sm bg-base-100 z-100 w-64 shadow-md right-0">
+                                    <div className="card-body">
+                                        <p className='font-semibold text-xl'>{user.displayName}</p>
+                                        <p>Email : {user.email}</p>
+                                        <button onClick={handleLogout} className="btn bg-red-700 text-base-100">Logout</button>
+                                    </div>
+                                </div>
+                            </div>
+                        }
+                        <div className="theme flex items-center gap-2">
+                            <MdLightMode /><input onChange={(e) => handleTheme(e.target.checked)} checked={theme === 'dark' ? 'checked' : ''} type="checkbox" className="toggle" /><MdDarkMode />
+                        </div>
+                    </div>
                 </nav>
                 {/* Page content here */}
                 <div className="p-4">
@@ -109,7 +157,7 @@ const DashboardLayout = () => {
                             </Link>
                         </li>
                         <li>
-                            <Link to={"/dashboard/profile/"+ _id} className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Profile">
+                            <Link to={"/dashboard/profile/" + _id} className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Profile">
                                 <FaUserCircle />
                                 <span className="is-drawer-close:hidden">Profile</span>
                             </Link>
